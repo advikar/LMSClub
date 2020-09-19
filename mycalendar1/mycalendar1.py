0149cb127cd31faa67a7c2929f12a5035327a097
advikar <70820122+advikar@users.noreply.github.com>
2020-09-19 13:21:27 -0700

    Create mycalendar1.py
    Adding my mycalender1.py file

---
+++ mycalendar1/mycalendar1.py
@@ -0,0 +1,10 @@
+# This is my school calendar code that I repersented with the variable lmsclub1
+lmsclub1 =  '''Day     Time            Subject
+               Monday  9:10-10:15 AM   Math
+                       10:35-11:40 AM  Science
+                       11:40-12:10 AM  Wheel
+               Tuesday 9:10-10:15 AM   P.F.
+                       10:35-11:40 AM  S.S.
+                       11:40-12:10 AM  L.A.'''
+#I printed my code
+print (lmsclub1)
