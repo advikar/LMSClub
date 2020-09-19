dc39091bd610a163f53eb4ab0a6b60636993b805
advikar <70820122+advikar@users.noreply.github.com>
2020-09-19 13:37:30 -0700

    Create mycalendar2.py

---
+++ mycalendar2/mycalendar2.py
@@ -0,0 +1,13 @@
+#This is my school calendar code that I wrote using a function
+def printCalendar():
+	lmsclub2 = '''Day     Time            Subject
+                Monday  9:10-10:15 AM   Math
+                        10:35-11:40 AM  Science
+                        11:40-12:10 AM  Wheel
+                Tuesday 9:10-10:15 AM   P.F.
+                        10:35-11:40 AM  S.S.
+                        11:40-12:10 AM  L.A.'''
+	print (lmsclub2) 
+
+#Calling function to print my calendar code
+printCalendar()
